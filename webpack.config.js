d4b5ba1de138bc9dc418af481b9d2f81419046fa
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2019-12-20 08:39:17 +0100

    Bump clean-webpack-plugin to v3.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,83 @@
 const path = require('path');
 const webpack = require('webpack');
 const BabelEnginePlugin = require('babel-engine-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 let config = {
-	entry: {
-		plugin: './src/plugin.js'
-	},
-	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, '_build/browser')
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: /node_modules/,
-				use: 'umd-compat-loader'
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							['babel-preset-env', { targets: { browsers: ['ie >= 11', 'safari >= 10'] } }]
-						]
-					}
-				}
-			}
-		]
-	},
-	plugins: [
-		new BabelEnginePlugin({
-			presets: [['env', { targets: { browsers: ['ie >= 11', 'safari >= 10'] } }]],
-		}),
- 		new CleanWebpackPlugin(['_build']),
-		new CopyWebpackPlugin([
-			{ from: path.resolve('LICENSE'), to: path.resolve('_build/') },
-			{ from: path.resolve('package.json'), to: path.resolve('_build/') },
-			{ from: path.resolve('README.md'), to: path.resolve('_build/') },
-			{ from: path.resolve('src/interface/cucumber.js'), to: path.resolve('_build/interface/') },
-			{ from: path.resolve('src/plugin.js'), to: path.resolve('_build/') },
-			{ from: path.resolve('src/plugin.d.ts'), to: path.resolve('_build/') }
-		])
-  	],
-	stats: {
-		assets: false,
-		entrypoints: true,
-		errors: true,
-		hash: false,
-		modules: false,
-		version: false,
-		warnings: true
-	},
-	resolve: {
-		alias: {
-			cucumber: 'cucumber/dist/cucumber'
-		},
-		extensions: ['.ts', '.js']
-	}
+  entry: {
+    plugin: './src/plugin.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '_build/browser')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /node_modules/,
+        use: 'umd-compat-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                'babel-preset-env',
+                { targets: { browsers: ['ie >= 11', 'safari >= 10'] } }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new BabelEnginePlugin({
+      presets: [
+        ['env', { targets: { browsers: ['ie >= 11', 'safari >= 10'] } }]
+      ]
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [path.join(process.cwd(), '_build')]
+    }),
+    new CopyWebpackPlugin([
+      { from: path.resolve('LICENSE'), to: path.resolve('_build/') },
+      { from: path.resolve('package.json'), to: path.resolve('_build/') },
+      { from: path.resolve('README.md'), to: path.resolve('_build/') },
+      {
+        from: path.resolve('src/interface/cucumber.js'),
+        to: path.resolve('_build/interface/')
+      },
+      { from: path.resolve('src/plugin.js'), to: path.resolve('_build/') },
+      { from: path.resolve('src/plugin.d.ts'), to: path.resolve('_build/') }
+    ])
+  ],
+  stats: {
+    assets: false,
+    entrypoints: true,
+    errors: true,
+    hash: false,
+    modules: false,
+    version: false,
+    warnings: true
+  },
+  resolve: {
+    alias: {
+      cucumber: 'cucumber/dist/cucumber'
+    },
+    extensions: ['.ts', '.js']
+  }
 };
 if (
-	process.env['NODE_ENV'] === 'production' ||
-	process.env['INTERN_BUILD'] === 'release'
+  process.env['NODE_ENV'] === 'production' ||
+  process.env['INTERN_BUILD'] === 'release'
 ) {
-	config.plugins = [new webpack.optimize.UglifyJsPlugin()];
+  config.plugins = [new webpack.optimize.UglifyJsPlugin()];
 }
 module.exports = config;
